edf6307b379ed3e9704da9f8d4e596b0e1f64f3e
Harsh Vardhan Yadav <harshvardhanyadav2003lmp@gmail.com>
2025-12-27 17:46:51 +0530

    fix: fix reactions persistance path alias typo

--- apps/reaction/src/infrastructure/anti-corruption/index.ts
+++ apps/reaction/src/infrastructure/anti-corruption/index.ts
@@ -1,3 +1,3 @@
 export * from './aggregate-persistance-acl/reaction-persistance.acl';
-export * from './reaction-status-acl/grpc-persistance-reaction-status.enum-mapper';
+export * from './reaction-status-acl/domain-transport.mapper';
 export * from './reaction-status-acl/transport-domain.mapper';
