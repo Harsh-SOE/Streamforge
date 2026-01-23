ded92105a48ac4cfeb53743067c41e03ab017be4
Harsh Vardhan Yadav <harshvardhanyadav2003lmp@gmail.com>
2026-01-23 20:04:38 +0530

    chore: remove uneccesary comments

--- libs/common/src/events/topics.ts
+++ libs/common/src/events/topics.ts
@@ -2,9 +2,3 @@ export const INTERNAL_BUFFER = 'aggregate.buffer';
 export const AGGREGATE_EVENT = 'aggregate.integration.events';
 export const PROJECTION_EVENT = 'projection.integration.events';
 export const INTERACTION_EVENTS = 'interactions';
-
-// export const USER_EVENTS = 'users';
-// export const CHANNEL_EVENTS = 'channels';
-// export const VIDEO_EVENTS = 'videos';
-
-// export const SYSTEM_EVENTS = 'system';
